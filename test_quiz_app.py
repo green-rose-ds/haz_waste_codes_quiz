395a123625e2e0e5c51f6b408d3b410bf336ff5d
[green-rose-ds] <[roberts.rosea@nulondon.ac.uk]>
2026-05-22 13:43:50 +0100

    added failing tests for the name validation logic I will write first

--- test_quiz_app.py
+++ test_quiz_app.py
@@ -1,3 +1,18 @@
+import quiz_dictionary
+from quiz_logic import QuizLogic
+
 def test_smoke():
     assert 1 + 1 == 2
 
+def test_dictionary_items():
+    assert len(quiz_dictionary.quiz_questions) == 16
+
+def test_name_is_not_blank():
+    assert QuizLogic.player_name("") == False
+
+def test_name_length():
+    assert QuizLogic.player_name("r") == False
+    assert QuizLogic.player_name("rosieeeeeeeeeeeeeeeee") == False 
+
+def test_name_is_alpha():
+    assert QuizLogic.player_name("rosi3") == False
